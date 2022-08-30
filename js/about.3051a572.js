013b03cff3f84f4b2f1ff4c876d2e9620d2d4ce3
Jordan Cotter <jrdn.cttr@gmail.com>
2022-08-30 17:44:41 -0500

    Updates

--- js/about.3051a572.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self["webpackChunkcchs_fe"]=self["webpackChunkcchs_fe"]||[]).push([[443],{242:function(n,c,e){e.r(c),e.d(c,{default:function(){return o}});var s=e(252);const u={class:"about"},t=(0,s._)("h1",null,"This is an about page",-1),a=[t];function r(n,c){return(0,s.wg)(),(0,s.iD)("div",u,a)}var f=e(744);const i={},h=(0,f.Z)(i,[["render",r]]);var o=h}}]);
-//# sourceMappingURL=about.3051a572.js.map
